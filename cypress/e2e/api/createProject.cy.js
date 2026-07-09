92ec6f043f68a68d14a989d1f26f532b920f1c78
Marcos <velosoamarcos@gmail.com>
2026-07-09 15:09:34 -0300

    test(projeto): Adiciona o teste cria projeto usando API

---
+++ cypress/e2e/api/createProject.cy.js
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker'
+
+describe('Create Project', () => {  
+    it('Deve criar um project no gitlab com sucesso', () => {
+      const project = {
+        name: `Project-${faker.datatype.uuid()}`,
+        description: faker.random.words(5)
+
+      }
+
+      cy.api_createProject(project)
+        .then(response => {
+          expect(response.status).to.equal(201)
+          expect(response.body.name).to.equal(project.name)  
+          expect(response.body.description).to.equal(project.description)  
+
+        })
+
+    })
+
+})
